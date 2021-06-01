a8c59a9a8ab5d6ab6303cbd1a07e6c183aea38a3
Niklas Brocker <niklas@Niklass-MBP.fritz.box>
2021-06-01 18:25:46 +0200

    test

--- main.py
+++ main.py
@@ -75,6 +75,8 @@ if __name__ == '__main__':
     """
     Können leider nicht die Library verwenden, weil sie scheinbar Probleme hat und weil wir sämtliche Informationen
     wie Kommentare behalten wollen, für weitere Informationen in der Zukunft.
+    
+    Test
     """
     project = DjangoProject('django_sample_project.apps.config.settings')
     a = project.get_models(include_django=True, as_interface=True)
