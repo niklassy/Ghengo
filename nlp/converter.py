d96a69cdd8600567e99b95f12a7af92a7ab4a3aa
Niklas Brocker <niklas@Niklass-MBP.fritz.box>
2021-06-15 13:45:49 +0200

    moved extractors into a property

--- nlp/converter.py
+++ nlp/converter.py
@@ -69,7 +69,7 @@ class ModelFactoryConverter(Converter):
         self._model = None
         self._model_token = None
         self._variable_name = None
-        self._extractors = []
+        self._extractors = None
 
     def get_statement_kwargs(self, *args, **kwargs):
         factory_statement = ModelFactoryExpression(*args, **kwargs)
@@ -109,7 +109,7 @@ class ModelFactoryConverter(Converter):
         column_names = datatable.get_column_names()
 
         for row in datatable.rows:
-            extractors_copy = self._extractors.copy()
+            extractors_copy = self.extractors.copy()
 
             for index, cell in enumerate(row.cells):
                 field_searcher = ModelFieldSearcher(column_names[index], src_language=self.language)
@@ -194,82 +194,88 @@ class ModelFactoryConverter(Converter):
             field_searcher_root = ModelFieldSearcher(text=str(token.lemma_), src_language=self.language)
             return field_searcher_root.search(model_interface=self.model_interface)
 
-    def get_extractors(self):
-        fields = []
-        non_stop_tokens = get_non_stop_tokens(self.document)
+    @property
+    def extractors(self):
+        """
+        Returns all the extractors. Each extractor gets the information about each field in the model factory
+        statement.
+        """
+        if self._extractors is None:
+            fields = []
+            non_stop_tokens = get_non_stop_tokens(self.document)
 
-        handled_non_stop = []
-        unhandled_noun_chunks = self.get_noun_chunks()[1:].copy()
-        unhandled_propn_chunk = None
-        for non_stop_token in non_stop_tokens:
-            if non_stop_token.i < self.get_noun_chunks()[0].end or non_stop_token.head == self.model_token:
-                handled_non_stop.append(non_stop_token)
-                continue
+            handled_non_stop = []
+            unhandled_noun_chunks = self.get_noun_chunks()[1:].copy()
+            unhandled_propn_chunk = None
+            for non_stop_token in non_stop_tokens:
+                if non_stop_token.i < self.get_noun_chunks()[0].end or non_stop_token.head == self.model_token:
+                    handled_non_stop.append(non_stop_token)
+                    continue
 
-            # there are two ways to detect a field:
-            #   1) by noun chunks
-            #   2) by using the stop tokens
-            #       a token references another which can be extracted by the `head` attribute
+                # there are two ways to detect a field:
+                #   1) by noun chunks
+                #   2) by using the stop tokens
+                #       a token references another which can be extracted by the `head` attribute
 
-            head = non_stop_token.head
-            try:
-                current_noun_chunk = unhandled_noun_chunks[0]
-            except IndexError:
-                continue
+                head = non_stop_token.head
+                try:
+                    current_noun_chunk = unhandled_noun_chunks[0]
+                except IndexError:
+                    continue
 
-            # clean the noun chunks
-            if non_stop_token in current_noun_chunk:
-                if non_stop_token == current_noun_chunk[-1]:
+                # clean the noun chunks
+                if non_stop_token in current_noun_chunk:
+                    if non_stop_token == current_noun_chunk[-1]:
+                        unhandled_noun_chunks.remove(current_noun_chunk)
+
+                    if current_noun_chunk.root.pos_ == 'PROPN':
+                        unhandled_propn_chunk = current_noun_chunk
+                        continue
+                elif current_noun_chunk[-1] not in non_stop_tokens:
                     unhandled_noun_chunks.remove(current_noun_chunk)
 
-                if current_noun_chunk.root.pos_ == 'PROPN':
-                    unhandled_propn_chunk = current_noun_chunk
+                if non_stop_token in handled_non_stop:
                     continue
-            elif current_noun_chunk[-1] not in non_stop_tokens:
-                unhandled_noun_chunks.remove(current_noun_chunk)
 
-            if non_stop_token in handled_non_stop:
-                continue
+                if head in non_stop_tokens and head not in handled_non_stop:
+                    field_token = head
+                    field_value_token = non_stop_token
+                elif unhandled_propn_chunk:
+                    field_token = non_stop_token
+                    field_value_token = unhandled_propn_chunk.root
+                elif current_noun_chunk.root.pos_ == 'NOUN':
+                    field_token = current_noun_chunk.root
+                    field_value_token = None
 
-            if head in non_stop_tokens and head not in handled_non_stop:
-                field_token = head
-                field_value_token = non_stop_token
-            elif unhandled_propn_chunk:
-                field_token = non_stop_token
-                field_value_token = unhandled_propn_chunk.root
-            elif current_noun_chunk.root.pos_ == 'NOUN':
-                field_token = current_noun_chunk.root
-                field_value_token = None
+                    for token in current_noun_chunk:
+                        if is_proper_noun_of(token, field_token):
+                            field_value_token = token
+                            break
+                else:
+                    continue
 
-                for token in current_noun_chunk:
-                    if is_proper_noun_of(token, field_token):
-                        field_value_token = token
-                        break
-            else:
-                continue
+                noun_chunk = get_noun_chunk_of_token(field_token, self.document)
+                field = self._search_for_field(span=noun_chunk, token=field_token)
 
-            noun_chunk = get_noun_chunk_of_token(field_token, self.document)
-            field = self._search_for_field(span=noun_chunk, token=field_token)
+                handled_non_stop.append(head)
+                handled_non_stop.append(non_stop_token)
+                unhandled_propn_chunk = None
 
-            handled_non_stop.append(head)
-            handled_non_stop.append(non_stop_token)
-            unhandled_propn_chunk = None
+                if field in [f for f, _, _ in fields]:
+                    continue
 
-            if field in [f for f, _, _ in fields]:
-                continue
+                fields.append((field, field_token, field_value_token))
 
-            fields.append((field, field_token, field_value_token))
+            output = []
+            for field, field_token, value_token in fields:
+                output.append(
+                    ModelFieldExtractor(self.test_case, value_token, field_token, self.model_interface, field))
 
-        output = []
-        for field, field_token, value_token in fields:
-            output.append(ModelFieldExtractor(self.test_case, value_token, field_token, self.model_interface, field))
-
-        return output
+            self._extractors = output
+        return self._extractors
 
     def convert_to_statements(self):
-        self._extractors = self.get_extractors()
-
         if not self.related_object.has_datatable:
-            return self.get_statements(self._extractors)
+            return self.get_statements(self.extractors)
 
         return self._get_statements_with_datatable()
