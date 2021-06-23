4d310744ef3794223da7dfdc745a3a5877ab6a99
Niklas Brocker <niklas@Niklass-MBP.fritz.box>
2021-06-23 19:57:43 +0200

    fxied tests

--- conftest.py
+++ conftest.py
@@ -2,13 +2,16 @@ import pytest
 
 from django_meta.project import DjangoProject
 from gherkin.settings import Settings
+from nlp.generate.settings import GenerationSettings
 
 
 @pytest.fixture(autouse=True)
 def run_around_tests():
     """For now the settings are used for the language, in some tests that language may be changed, so reset it here."""
     Settings.language = Settings.DEFAULT_LANGUAGE
+    GenerationSettings.test_type = None
     yield
+    GenerationSettings.test_type = GenerationSettings.DEFAULT_TEST_TYPE
     Settings.language = Settings.DEFAULT_LANGUAGE
 
 
