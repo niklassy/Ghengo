326051a300c52ec91fc1b312f96187bb1f79aba8
Niklas Brocker <niklas@Niklass-MBP.fritz.box>
2021-06-25 11:27:33 +0200

    implemented api view interface

--- django_meta/project.py
+++ django_meta/project.py
@@ -13,7 +13,9 @@ class UrlPatternInterface(object):
     def __init__(self, url_pattern):
         self.url_pattern = url_pattern
         self._view_set = None
+        self._api_view = None
         self._view_set_determined = False
+        self._api_view_determined = False
 
     @property
     def reverse_name(self):
@@ -29,53 +31,99 @@ class UrlPatternInterface(object):
 
     @property
     def methods(self):
-        if self.view_set is None:
+        if self.view_set is None and self.api_view is None:
             return []
 
         methods = []
+        if self.api_view is not None:
+            for _, method, url_name in self.api_view.get_all_actions():
+                if method not in methods:
+                    methods.append(method)
+
+            return methods
+
         for _, method, url_name in self.view_set.get_all_actions():
             if url_name == self.url_name and method not in methods:
                 methods.append(method)
 
         return methods
 
+    def _get_view_cls(self):
+        lookup_str = self.url_pattern.lookup_str
+
+        full_name_as_list = lookup_str.split('.')
+        full_name_as_list.reverse()
+
+        view_name = []
+        view_path = lookup_str.split('.')
+        module = None
+
+        for lookup_part in full_name_as_list:
+            view_name.append(lookup_part)
+            view_path = view_path[:-1]
+
+            try:
+                module = importlib.import_module('.'.join(view_path))
+                break
+            except ModuleNotFoundError:
+                continue
+        view_name.reverse()
+        return getattr(module, view_name[0])
+
+    @property
+    def api_view(self):
+        if self._api_view_determined is False:
+            from rest_framework.views import APIView
+            from rest_framework.viewsets import GenericViewSet
+            from rest_framework.routers import APIRootView
+            view_cls = self._get_view_cls()
+
+            if inspect.isclass(view_cls) and issubclass(view_cls, APIView) and view_cls != APIRootView and not issubclass(view_cls, GenericViewSet):
+                self._api_view = ApiViewInterface(view_cls(request=None, format_kwargs=None))
+            else:
+                self._api_view = None
+
+            self._api_view_determined = True
+        return self._api_view
+
     @property
     def view_set(self):
         if self._view_set_determined is False:
-            from rest_framework.views import APIView
             from rest_framework.viewsets import GenericViewSet
+            view_cls = self._get_view_cls()
 
-            lookup_str = self.url_pattern.lookup_str
-
-            full_name_as_list = lookup_str.split('.')
-            full_name_as_list.reverse()
-
-            view_name = []
-            view_path = lookup_str.split('.')
-            module = None
-
-            for lookup_part in full_name_as_list:
-                view_name.append(lookup_part)
-                view_path = view_path[:-1]
-
-                try:
-                    module = importlib.import_module('.'.join(view_path))
-                    break
-                except ModuleNotFoundError:
-                    continue
-            view_name.reverse()
-            view_cls = getattr(module, view_name[0])
-
-            from rest_framework.routers import APIRootView
-            if view_cls == APIRootView:
-                self._view_set = None
-
-            # TODO: handle APIView and @api_view()
-            elif not inspect.isclass(view_cls) or not issubclass(view_cls, (APIView, GenericViewSet)):
-                self._view_set = None
-            else:
+            if inspect.isclass(view_cls) and issubclass(view_cls, GenericViewSet):
                 self._view_set = ViewSetInterface(view_cls(request=None, format_kwarg=None))
+            else:
+                self._view_set = None
+
+            self._view_set_determined = True
+
         return self._view_set
+
+
+class ApiViewInterface(object):
+    def __init__(self, api_view):
+        self.api_view = api_view
+
+    @property
+    def methods(self):
+        return [method.lower() for method in self.api_view.allowed_methods if method.lower() != 'options']
+
+    def get_all_actions(self):
+        possible_actions = [
+            ('get', 'get', ''),
+            ('post', 'post', ''),
+            ('patch', 'patch', ''),
+            ('put', 'put', ''),
+        ]
+
+        actions = []
+        for fn_name, method, url_name in possible_actions:
+            if hasattr(self.api_view, fn_name):
+                actions.append((fn_name, method, url_name))
+
+        return actions
 
 
 class ViewSetInterface(object):
@@ -83,10 +131,6 @@ class ViewSetInterface(object):
         self.view_set = view_set
 
     def get_all_actions(self):
-        from rest_framework.routers import APIRootView
-        if isinstance(self.view_set, APIRootView):
-            return [('get', 'get')]
-
         extra_actions = [
             extra_action for extra_action in self.view_set.get_extra_actions()
         ]
