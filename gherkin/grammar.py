a683260afce38645c8630007687114db3da54c83
Niklas Brocker <niklas@Niklass-MBP.fritz.box>
2021-08-23 08:48:20 +0200

    removed test code

--- gherkin/grammar.py
+++ gherkin/grammar.py
@@ -5,6 +5,3 @@ from gherkin.non_terminal import GherkinDocumentNonTerminal
 class GherkinGrammar(Grammar):
     start_non_terminal = GherkinDocumentNonTerminal()
 
-
-a = GherkinGrammar().to_ebnf()
-b = 1
